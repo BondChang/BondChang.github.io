5972574f0406014c535bd7faba8953246f3ebd47
BondChang <1451776535@qq.com>
2017-10-12 22:29:10 +0800

    上传网站源码到github

--- games/qedfc/js/config.main.js
+++
@@ -1,2 +0,0 @@
-dataForWeixin.appId = "wx8820cdf5db680ffa";
-dataForWeixin.url = "http://app.gzlankong.com/200game/"+_con["num"]+"/";
--- games/zhaonimei/js/wx4syt.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("WeixinJSBridgeReady",function(){if(window.WeixinJSBridge){var a=WeixinJSBridge.invoke;WeixinJSBridge.invoke=function(){arguments[2]=function(){window.location.href="http://app.gzlankong.com/200game"},a.apply(WeixinJSBridge,arguments)}}});
